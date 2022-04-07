a6561f59926831e2a6cb2f3bc64db3a2c4a6053f
Erik <103200349+WafIukas@users.noreply.github.com>
2022-04-07 22:43:40 +0300

    Add files via upload

---
+++ 1 labaratorinis/faktorialas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace faktorialas
+{
+    class fakt
+    {
+        public int n = 5;  
+        public int faktor = 1;   
+        public void sudetis2()
+        {
+
+            for (int i = 1; i <= n; i++) 
+            {
+                faktor = faktor * i;
+            }
+        }
+    }
+}
