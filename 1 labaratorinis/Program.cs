6e3188dfdb74d79f1d8441665aff25c1179acf97
Erik <103200349+WafIukas@users.noreply.github.com>
2022-04-07 21:02:21 +0300

    Add files via upload

---
+++ 1 labaratorinis/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace pirmalab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       
+            _1_labaratorinis.uzduotis myObj = new _1_labaratorinis.uzduotis();
+            myObj.sudetis();
+            Console.WriteLine("Galutinis skaicius : " + myObj.c);
+            Console.WriteLine("Elementas : " + myObj.n);
+
+            Console.ReadLine();
+        }
+    }
+
+}
+
