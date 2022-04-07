e2ba6d08be22428f4ee3442419d57220393365af
Erik <103200349+WafIukas@users.noreply.github.com>
2022-04-07 22:43:20 +0300

    Update Program.cs

--- 1 labaratorinis/Program.cs
+++ 1 labaratorinis/Program.cs
@@ -9,9 +9,12 @@ namespace pirmalab
         static void Main(string[] args)
         {       
             _1_labaratorinis.uzduotis myObj = new _1_labaratorinis.uzduotis();
+            faktorialas.fakt myObj2 = new faktorialas.fakt();
             myObj.sudetis();
+             myObj2.sudetis2();
             Console.WriteLine("Galutinis skaicius : " + myObj.c);
             Console.WriteLine("Elementas : " + myObj.n);
+            Console.WriteLine("Faktorialas is 5 yra : " + myObj2.faktor);
 
             Console.ReadLine();
         }
