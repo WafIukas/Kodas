9917dd6a45ca1ebc23c74b18a54a945edbce077d
Erik <103200349+WafIukas@users.noreply.github.com>
2022-04-07 22:40:59 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_labaratorinis
+{
+    class uzduotis
+    {
+        public double a = 1; //pirmasis sekos narys
+        public double b = 2; //antrasis sekos narys
+        public double c;  //trečiasis sekos narys
+        public int n = 2; //Kelintas sekos elementas yra skaiciuojamas
+        public double riba = Math.Pow(10, 15); //pakelia laipsniu
+        public void sudetis()
+        {
+
+            while (c <= riba)
+            {
+                c = a * b;
+                a = b;
+                b = c;
+                n++;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace pirmalab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       
+            _1_labaratorinis.uzduotis myObj = new _1_labaratorinis.uzduotis();
+            faktorialas.fakt myObj2 = new faktorialas.fakt();
+            myObj.sudetis();
+            myObj2.sudetis2();
+            Console.WriteLine("Galutinis skaicius : " + myObj.c);
+            Console.WriteLine("Elementas : " + myObj.n);
+            Console.WriteLine("Faktorialas is 5 yra : " + myObj2.faktor);
+
+            Console.ReadLine();
+        }
+    }
+
+}
+
